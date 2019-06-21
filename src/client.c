cc510cb7432342fda275c23385e0094ce463607a
Modesto Tamayo <modesto.t.tamayo@gmail.com>
2019-06-21 10:48:49 -0500

    completed the parse_url function

--- src/client.c
+++ src/client.c
@@ -34,20 +34,19 @@ urlinfo_t *parse_url(char *url)
 
   urlinfo_t *urlinfo = malloc(sizeof(urlinfo_t));
 
-  /*
-    We can parse the input URL by doing the following:
-
-    1. Use strchr to find the first slash in the URL (this is assuming there is no http:// or https:// in the URL).
-    2. Set the path pointer to 1 character after the spot returned by strchr.
-    3. Overwrite the slash with a '\0' so that we are no longer considering anything after the slash.
-    4. Use strchr to find the first colon in the URL.
-    5. Set the port pointer to 1 character after the spot returned by strchr.
-    6. Overwrite the colon with a '\0' so that we are just left with the hostname.
-  */
-
-  ///////////////////
-  // IMPLEMENT ME! //
-  ///////////////////
+  // 1. Use strchr to find the first slash in the URL (this is assuming there is no http:// or https:// in the URL).
+  // 2. Set the path pointer to 1 character after the spot returned by strchr.
+  path = strchr(hostname, '/');
+  urlinfo->path =  path + 1;
+  // 3. Overwrite the slash with a '\0' so that we are no longer considering anything after the slash.
+  hostname[(int)(path-hostname)] = '\0';
+  // 4. Use strchr to find the first colon in the URL.
+  port = strchr(hostname, ':');
+  // 5. Set the port pointer to 1 character after the spot returned by strchr.
+  urlinfo->port = port + 1;
+  // 6. Overwrite the colon with a '\0' so that we are just left with the hostname.
+  hostname[(int)(port-hostname)] = '\0';
+  urlinfo->hostname = hostname;
 
   return urlinfo;
 }
@@ -80,10 +79,10 @@ int main(int argc, char *argv[])
   int sockfd, numbytes;  
   char buf[BUFSIZE];
 
-  if (argc != 2) {
-    fprintf(stderr,"usage: client HOSTNAME:PORT/PATH\n");
-    exit(1);
-  }
+  // if (argc != 2) {
+  //   fprintf(stderr,"usage: client HOSTNAME:PORT/PATH\n");
+  //   exit(1);
+  // }
 
   /*
     1. Parse the input URL
@@ -96,6 +95,8 @@ int main(int argc, char *argv[])
   ///////////////////
   // IMPLEMENT ME! //
   ///////////////////
+  char *str = "localhost:3490/d20";
+  parse_url(str);
 
   return 0;
 }
